b349fe77bf5e23b582ef9fa198ae85d805512960
JPrimee <javierpsantos99@gmail.com>
2023-05-17 11:02:59 +0200

    first commit

--- hackathon/src/app/app.module.ts
+++ hackathon/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DropComponent } from './drop/drop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropComponent
   ],
   imports: [
     BrowserModule,
